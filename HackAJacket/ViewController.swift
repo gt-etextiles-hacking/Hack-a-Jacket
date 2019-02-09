7f2ea64670dca37f1be99bd965e88c5bd6e38e81
aayush <aayush.kumarmail@gmail.com>
2019-02-09 18:15:47 -0500

    code cleanup and logging button/logic

--- HackAJacket/ViewController.swift
+++ HackAJacket/ViewController.swift
@@ -3,6 +3,7 @@
 //  HackAJacket
 //
 //  Created by Caleb Rudnicki on 10/22/18.
+//  Edited by Aayush Kumar on 02/01/19
 //  Copyright © 2018 Caleb Rudnicki. All rights reserved.
 //
 
@@ -14,89 +15,111 @@ import MessageUI
 import MediaPlayer
 
 class ViewController: UIViewController {
-    
+
     let label = UILabel()
     let connectButton = PressableButton()
     let glowButton = PressableButton()
     let lightSwitch = UISwitch()
-    
+
     var centralManager: CBCentralManager!
     var peripheralObject: CBPeripheral!
     var peripheralList: [CBPeripheral] = []
     let uuid = UUID(uuidString: HAJString.hajJacketUUID)
-    var isButtonToConnect = true
+    var connected = false
     var seeThreads = true
+    var loggingThreadReadings = false
     var glowCharacteristic: CBCharacteristic!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         navigationItem.title = HAJString.hajTitle
-        
+
         view.addSubview(label)
         view.addSubview(connectButton)
         view.addSubview(glowButton)
         view.addSubview(lightSwitch)
-        
+
+        // status text
         label.autoCenterInSuperview()
         label.font = UIFont(name: "HelveticaNeueLight", size: 14)
         label.text = "No gesture detected yet..."
-        
+
+        // connect button UI definition (1/2)
         connectButton.autoMatch(.height, to: .height, of: view, withMultiplier: 0.1)
         connectButton.autoPinEdge(toSuperviewMargin: .leading)
         connectButton.autoPinEdge(toSuperviewMargin: .trailing)
         connectButton.autoPinEdge(.bottom, to: .bottom, of: view, withOffset: -8)
-        
+
+        // connect button UI definition (2/2)
         connectButton.colors = .init(button: HAJColor.hajBright, shadow: HAJColor.hajDark)
         connectButton.shadowHeight = 5
         connectButton.cornerRadius = 5
         connectButton.setTitle("Connect", for: .normal)
         connectButton.addTarget(self, action: #selector(connectButtonTapped), for: .touchUpInside)
-        
+
+        // glow button UI definition (1/2)
         glowButton.autoMatch(.height, to: .height, of: connectButton)
         glowButton.autoMatch(.width, to: .width, of: connectButton)
         glowButton.autoPinEdge(toSuperviewMargin: .leading)
         glowButton.autoPinEdge(toSuperviewMargin: .trailing)
         glowButton.autoPinEdge(.bottom, to: .top, of: connectButton, withOffset: -8)
-        
+
+        // glow button UI definition (2/2)
         glowButton.colors = .init(button: HAJColor.hajBright, shadow: HAJColor.hajDark)
         glowButton.shadowHeight = 5
         glowButton.cornerRadius = 5
         glowButton.setTitle("Glow", for: .normal)
         glowButton.addTarget(self, action: #selector(glowButtonTapped), for: .touchUpInside)
-        
-        let rightAddBarButtonItem = UIBarButtonItem(title: "See Gestures", style: UIBarButtonItem.Style.plain, target: self, action: #selector(ViewController.addTapped))
+
+        let rightAddBarButtonItem = UIBarButtonItem(title: "See Gestures", style: UIBarButtonItem.Style.plain, target: self, action: #selector(ViewController.gestureReadingToggleHandler))
         navigationItem.setRightBarButton(rightAddBarButtonItem, animated: true)
         
-        isButtonToConnect == true ? centralManager = CBCentralManager(delegate: self, queue: nil) : centralManager.cancelPeripheralConnection(peripheralObject)
+        let leftAddBarButtonItem = UIBarButtonItem(title: "Start Logging", style: UIBarButtonItem.Style.plain, target: self, action: #selector(ViewController.loggingToggleHandler))
+        navigationItem.setLeftBarButton(leftAddBarButtonItem, animated: true)
+
+        !connected ? centralManager = CBCentralManager(delegate: self, queue: nil) : centralManager.cancelPeripheralConnection(peripheralObject)
     }
     
+    @objc func loggingToggleHandler (sender:UIButton) {
+        
+        let leftAddBarButtonItem = UIBarButtonItem(title: "\(loggingThreadReadings ? "Start" : "Stop") Logging", style: UIBarButtonItem.Style.plain, target: self, action: #selector(ViewController.loggingToggleHandler))
+        navigationItem.setLeftBarButton(leftAddBarButtonItem, animated: true)
+        loggingThreadReadings = !loggingThreadReadings
+
+    }
+
 //    event handler for the top right toggle between "See Gestures" and "See Threads"
-    @objc func addTapped (sender:UIButton) {
+    @objc func gestureReadingToggleHandler (sender:UIButton) {
         centralManager.cancelPeripheralConnection(peripheralObject)
-        let rightAddBarButtonItem = seeThreads ? UIBarButtonItem(title: "See Threads", style: UIBarButtonItem.Style.plain, target: self, action: #selector(ViewController.addTapped)) : UIBarButtonItem(title: "See Gestures", style: UIBarButtonItem.Style.plain, target: self, action: #selector(ViewController.addTapped))
+        let rightAddBarButtonItem = seeThreads ? UIBarButtonItem(title: "See Threads", style: UIBarButtonItem.Style.plain, target: self, action: #selector(ViewController.gestureReadingToggleHandler)) : UIBarButtonItem(title: "See Gestures", style: UIBarButtonItem.Style.plain, target: self, action: #selector(ViewController.gestureReadingToggleHandler))
         seeThreads = !seeThreads
         navigationItem.setRightBarButton(rightAddBarButtonItem, animated: true)
         centralManager = CBCentralManager(delegate: self, queue: nil)
     }
-    
+
     @objc func connectButtonTapped(_ sender: UIButton) {
-        isButtonToConnect == true ? centralManager = CBCentralManager(delegate: self, queue: nil) : centralManager.cancelPeripheralConnection(peripheralObject)
+        !connected ? centralManager = CBCentralManager(delegate: self, queue: nil) : centralManager.cancelPeripheralConnection(peripheralObject)
     }
-    
+
     @objc func glowButtonTapped(_ sender: UIButton) {
-        if isButtonToConnect == false {
-            let dataval = dataWithHexString(hex: "801308001008180BDA060A0810107830013801")
-            let dataval1 = dataWithHexString(hex: "414000")
-            peripheralObject.writeValue(dataval, for: glowCharacteristic, type: .withoutResponse)
-            peripheralObject.writeValue(dataval1, for: glowCharacteristic, type: .withoutResponse)
+        if connected {
+            glowTag()
         }
+    }
+
+    @objc func glowTag() {
+        // convert every 2 hex values to 1 byte
+        let dataval = dataWithHexString(hex: "801308001008180BDA060A0810107830013801") // 19 bytes, 152 bits: ...
+        let dataval1 = dataWithHexString(hex: "414000") // 3 bytes, 24 bits: 1000001 1000000 0000000
+        peripheralObject.writeValue(dataval, for: glowCharacteristic, type: .withoutResponse)
+        peripheralObject.writeValue(dataval1, for: glowCharacteristic, type: .withoutResponse)
     }
 
 }
 
 extension ViewController: CBCentralManagerDelegate {
-    
+
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         switch central.state {
         case .unknown:
@@ -109,9 +132,9 @@ extension ViewController: CBCentralManagerDelegate {
             print("Unauthorized")
         case .poweredOn:
             print("Powered On")
-            
+
             // helper function (1/2) call for finding the UUID for a new user
-//            centralManager.scanForPeripherals(withServices: [], options: nil) // CBUUID.init(nsuuid: uuid!)
+            // centralManager.scanForPeripherals(withServices: [], options: nil) // CBUUID.init(nsuuid: uuid!)
             peripheralList = centralManager.retrievePeripherals(withIdentifiers: [uuid!])
             peripheralObject = peripheralList[0]
             peripheralObject.delegate = self
@@ -120,62 +143,62 @@ extension ViewController: CBCentralManagerDelegate {
             print("Powered Off")
         }
     }
-    
+
     // helper function (2/2) for finding the UUID for a new user
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
         print("UUID(s) found: \(peripheral.name) - \(peripheral.identifier.uuidString)")
     }
-    
+
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
         peripheral.discoverServices(nil)
         connectButton.setTitle("Disconnect", for: .normal)
-        isButtonToConnect = false
+        connected = true
     }
-    
+
     func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
         connectButton.setTitle("Connect", for: .normal)
-        isButtonToConnect = true
+        connected = false
     }
 
 }
 
 extension ViewController: MFMessageComposeViewControllerDelegate {
-    
+
     func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
         self.dismiss(animated: true, completion: nil)
     }
-    
+
 }
 
 extension ViewController: CBPeripheralDelegate {
 
     func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
         guard let services = peripheral.services else { return }
-        
+
         for service in services {
             peripheral.discoverCharacteristics(nil, for: service)
         }
     }
-    
+
     func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
         guard let characteristics = service.characteristics else { return }
-        
+
         for characteristic in characteristics {
-            
+
             if characteristic.uuid.uuidString == HAJString.hajJacketUUID ||
                 (characteristic.uuid.uuidString == "D45C2030-4270-A125-A25D-EE458C085001" && seeThreads == false) ||
                 (characteristic.uuid.uuidString == "D45C2010-4270-A125-A25D-EE458C085001" && seeThreads == true)
             {
                 peripheral.setNotifyValue(true, for: characteristic)
             }
-            
+
             if characteristic.properties.contains(.writeWithoutResponse) {
                 print("\(characteristic.uuid): properties contains .writeWithResponse")
                 glowCharacteristic = characteristic
             }
         }
     }
-    
+
     func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
             // some async process?
             DispatchQueue.main.async() {
@@ -184,43 +207,18 @@ extension ViewController: CBPeripheralDelegate {
                 }
             }
         
-            if seeThreads == true {
-                return
-            }
-            let gestureString = decodeGesture(from: characteristic)
-            label.text = gestureString
-        
-            // 4, 5, or 6 Response: Glow
-            if gestureString == HAJString.hajGestureUndefined {
-                // convert every 2 hex values to 1 byte
-                let dataval2 = dataWithHexString(hex: "801308001008180BDA060A0810107830013801") // 19 bytes, 152 bits
-                let dataval3 = dataWithHexString(hex: "414000") // 3 bytes, 24 bits: 1000001 1000000 0000000
-                peripheralObject.writeValue(dataval2, for: glowCharacteristic, type: .withoutResponse)
-                peripheralObject.writeValue(dataval3, for: glowCharacteristic, type: .withoutResponse)
+            if !seeThreads {
+                // print decoded/identified gestures
+                let gestureString = decodeGesture(from: characteristic)
+                label.text = gestureString
                 
-            // Brush out Response
+                // AND glow if gesture 4, 5, or 6 is detected
+                if gestureString == HAJString.hajGestureUndefined {
+                    self.glowTag()
+                }
             }
-//            else if gestureString == HAJString.hajGestureBrushOut {
-//                let numberString = "6179810873"
-//                if let url = URL(string: "telprompt://\(numberString)"), UIApplication.shared.canOpenURL(url) {
-//                    if #available(iOS 10, *) {
-//                        UIApplication.shared.open(url)
-//                    } else {
-//                        UIApplication.shared.openURL(url)
-//                    }
-//                }
-//            // Brush in Response
-//            } else if gestureString == HAJString.hajGestureBrushIn {
-//                if (MFMessageComposeViewController.canSendText()) {
-//                    let controller = MFMessageComposeViewController()
-//                    controller.body = "Message Body"
-//                    controller.recipients = ["6179810873"]
-//                    controller.messageComposeDelegate = self
-//                    self.present(controller, animated: true, completion: nil)
-//                }
-//            }
     }
-    
+
     func dataWithHexString(hex: String) -> Data {
         var hex = hex
         var data = Data()
@@ -236,18 +234,22 @@ extension ViewController: CBPeripheralDelegate {
         return data
     }
 
+    // reads hex scaled pressure readings from each of the 16 threads
     private func findThread(from characteristic: CBCharacteristic) -> String {
         guard let characteristicData = characteristic.value else { return "Error" }
         let fullStr = characteristicData.hexEncodedString()
         let partStr = fullStr[20...35]
+        if self.loggingThreadReadings {
+            print(partStr)
+        }
         return partStr
     }
-    
-    // gestures are encoded with 9 status codes, this function resolves their designated names
+
+    // gestures are encoded with 9 status codes, this function resolves each to their designated names
     private func decodeGesture(from characteristic: CBCharacteristic) -> String {
         guard let characteristicData = characteristic.value,
             let byte = characteristicData.first else { return "Error" }
-        
+
         switch byte {
         case 0: return HAJString.hajGestureUndetected
         case 1: return HAJString.hajGestureDoubleTap
@@ -289,4 +291,5 @@ extension String {
         let end = index(startIndex, offsetBy: bounds.upperBound)
         return String(self[start..<end])
     }
+
 }
